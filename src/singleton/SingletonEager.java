a5c839caccb4753a6ea9cf009296d53af8b1165a
gabrielvictor0 <bielgvsa@gmail.com>
2025-06-06 01:38:33 -0300

    .

---
+++ src/singleton/SingletonEager.java
@@ -0,0 +1,15 @@
+package singleton;
+public class SingletonEager {
+     private static SingletonEager instancia;
+
+    private SingletonEager(){
+        super();
+    }
+
+    public static SingletonEager getInstancia(){
+        if(instancia == null){
+            instancia = new SingletonEager();
+        }
+        return instancia;
+    }
+}
